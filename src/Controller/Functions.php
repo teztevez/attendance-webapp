d93c9c5d5988d14c0ac632e0d6453c9e0b6752d6
Terry <B00100763@student.itb.ie>
2019-04-22 19:29:46 +0100

    Admin login created

--- src/Controller/Functions.php
+++ src/Controller/Functions.php
@@ -17,8 +17,21 @@ class Functions extends AbstractController
 	* @Route("/functions", name="catch") methods=("GET", "POST")
 	*/
 	public function index()
-	{	
-	
+	{
+		$request = Request::createFromGlobals(); 
+		
+		$type = $request->request->get('type', 'none');
+		
+		if($type == 'login') {
+				//get username and password
+			$username = $request->request->get('username', 'none');
+			$password = $request->request->get('password', 'none');		
+			
+			$repo = $this->getDoctrine()->getRepository(Admin::class); //type of entityManager
+			
+			$admin = $repo->findOneBy(['username' => $username, 'password' => $password]);			
+			
+			return new Response("Welcome " $admin->getUsername());	
 	}
 }
 ?>
